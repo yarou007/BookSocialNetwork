54f77f7f3385bb495bf9029e4712978b5a93b957
Yacine Hammi <yacine.hammi@esen.tn>
2024-09-03 23:02:26 +0100

    Docker works just fine dude that's awesome

--- book-network/src/main/java/com/yarou/book/BookNetworkApplication.java
+++ book-network/src/main/java/com/yarou/book/BookNetworkApplication.java
@@ -1,7 +1,11 @@
 package com.yarou.book;
 
+import com.yarou.book.role.Role;
+import com.yarou.book.role.roleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -14,4 +18,14 @@ public class BookNetworkApplication {
 		SpringApplication.run(BookNetworkApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner (roleRepository RoleRepository){
+		return args -> {
+			if (RoleRepository.findByName("USER").isEmpty()){
+				RoleRepository.save(
+						Role.builder().name("USER").build()
+				);
+			}
+		};
+	}
 }
