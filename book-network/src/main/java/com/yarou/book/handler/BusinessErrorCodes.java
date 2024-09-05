2001cad19793dc3b265b55cc1e1c2a1aefcbdba8
Yacine Hammi <yacine.hammi@esen.tn>
2024-09-05 21:43:36 +0100

    Exception handler flow implementation

---
+++ book-network/src/main/java/com/yarou/book/handler/BusinessErrorCodes.java
@@ -0,0 +1,36 @@
+package com.yarou.book.handler;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+
+public enum BusinessErrorCodes {
+
+   NO_CODE(0, NOT_IMPLEMENTED,"No code"),
+    INCORRECT_CURRENT_PASSWORD(300,BAD_REQUEST,"Current password is incorrect"),
+    NEW_PASSWORD_DOES_NOT_MATCH(301,BAD_REQUEST,"New password does not match"),
+    ACCOUNT_LOCKED(302,FORBIDDEN,"User account is locked"),
+
+    ACCOUNT_DISABLED(303,FORBIDDEN,"User account is disabled"),
+    BAD_CREDENTIALS(304,FORBIDDEN,"Login / Password is incorrect"),
+
+            ;
+
+
+
+    @Getter
+    private final int code;
+    @Getter
+    private final String description;
+    @Getter
+    private final HttpStatus httpStatus;
+
+    BusinessErrorCodes(int code,HttpStatus httpStatus, String description ) {
+        this.code = code;
+        this.description = description;
+        this.httpStatus = httpStatus;
+    }
+}
---
+++ book-network/src/main/java/com/yarou/book/handler/ExceptionResponse.java
@@ -0,0 +1,30 @@
+package com.yarou.book.handler;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+
+public class ExceptionResponse {
+
+    private Integer businessErrorCode; // error code asgined to each exception
+
+    private String businessErrorDescription;
+
+    private String error;
+
+    private Set<String> validationErrors;
+
+    private Map<String,String> errors;
+
+
+}
